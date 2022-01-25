5bbcfab1216d38409ef59c886131c447b3eaf464
Andy Chung <andy6756@gmail.com>
2022-01-25 14:47:33 +0800

    英雄之旅-2._顯示英雄列表

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,3 +1,4 @@
+import { HEROES } from './../mock-heroes';
 import { Hero } from './../hero';
 import { Component, OnInit } from '@angular/core';
 
@@ -9,13 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  hero : Hero = {
-    id: 1,
-    name: 'IronMan'
-  }
+
+  heroes = HEROES
+  selectedHero?: Hero;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero:Hero): void{
+    this.selectedHero = hero;
   }
 
 }
